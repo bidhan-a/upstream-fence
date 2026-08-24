b99ee4eabf97fb66b27797d77a8a47603b782a2d
bidhan-a <bidhan619@gmail.com>
2026-08-24 20:47:51 +0545

    add test program for rodata relocation

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,32 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     unsafe { core::hint::unreachable_unchecked() }
 }
 
+#[repr(transparent)]
+struct Pubkey([u8; 32]);
+
+static AUTH0: Pubkey = Pubkey([0x11; 32]);
+static AUTH1: Pubkey = Pubkey([0x22; 32]);
+static AUTH2: Pubkey = Pubkey([0x33; 32]);
+static AUTH3: Pubkey = Pubkey([0x44; 32]);
+
+#[used]
+static REGISTRY: [&Pubkey; 4] = [&AUTH0, &AUTH1, &AUTH2, &AUTH3];
+
 #[unsafe(no_mangle)]
-pub fn entrypoint(_input: *mut u8) -> u64 {
-    let mut var_a: u64 = 0x1111_2222_3333_4444;
-    let mut var_b: u64 = 0xAAAA_BBBB_CCCC_DDDD;
+pub fn entrypoint(input: *mut u8) -> u64 {
+    // ix data starts at offset 16
+    let index = unsafe { core::ptr::read_volatile(input.add(16)) } as usize;
 
-    core::hint::black_box(&mut var_a);
-    core::hint::black_box(&mut var_b);
+    let registry = core::ptr::addr_of!(REGISTRY).cast::<*const Pubkey>();
+    let key = unsafe { core::ptr::read_volatile(registry.add(index)) };
+    let val = unsafe { core::ptr::read_volatile(key.cast::<u8>()) };
+    let expected = [0x11u8, 0x22, 0x33, 0x44][index];
 
-    // This should lower via __multi3 on BPF
-    let prod = match var_a.checked_mul(var_b) {
-        Some(v) => v,
-        None => return 0,
-    };
-
-    prod ^ var_b
+    if val == expected {
+        0x0
+    } else {
+        0x1
+    }
 }
 
 #[cfg(test)]
@@ -29,19 +40,21 @@ mod tests {
     use solana_instruction::Instruction;
 
     #[test]
-    pub fn hello_world() {
+    fn registry_all_indices() {
         let mollusk = Mollusk::new(
             &[2u8; 32].into(),
             "target/bpfel-unknown-none/release/libupstream_fence",
         );
-        mollusk.process_and_validate_instruction(
-            &Instruction {
-                program_id: [2u8; 32].into(),
-                accounts: vec![],
-                data: vec![],
-            },
-            &vec![],
-            &[Check::success()],
-        );
+        for index in 0..4u8 {
+            mollusk.process_and_validate_instruction(
+                &Instruction {
+                    program_id: [2u8; 32].into(),
+                    accounts: vec![],
+                    data: vec![index],
+                },
+                &vec![],
+                &[Check::success()],
+            );
+        }
     }
 }
